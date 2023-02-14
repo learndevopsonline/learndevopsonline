6f4ac3f802dee84d564fbad699b9203d53c25a87
Raghu K <raghuk.vit@gmail.com>
2023-02-14 15:34:26 +0530

    ss

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        }
+        },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.

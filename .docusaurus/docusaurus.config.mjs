8a5a137b997b4efad63f902bf3ab8935fb136289
SVR Kalluru <raghuk.vit@gmail.com>
2025-03-15 20:51:12 +0530

    ss

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -64,7 +64,7 @@ export default {
     "footer": {
       "style": "dark",
       "links": [],
-      "copyright": "Copyright © 2024 My Project, Inc. Built with Docusaurus."
+      "copyright": "Copyright © 2025 My Project, Inc. Built with Docusaurus."
     },
     "prism": {
       "theme": {
